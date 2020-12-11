7eb79a78bd29753dc0e874a84b2b55488295a1c9
xhlove <vvtoolbox.dev@gmail.com>
2020-12-12 00:38:50 +0800

    nothing

--- mpd_content_parser.py
+++ mpd_content_parser.py
@@ -1,7 +1,7 @@
 '''
 作者: weimo
 创建日期: 2020-09-14 13:13:18
-上次编辑时间: 2020-12-12 00:26:42
+上次编辑时间: 2020-12-12 00:38:39
 一个人的命运啊,当然要靠自我奋斗,但是...
 '''
 
@@ -169,7 +169,7 @@ The SegmentTimeline element shall contain a list of S elements each of which des
 of contiguous segments of identical MPD duration. The S element contains a mandatory @d attribute
 specifying the MPD duration, an optional @r repeat count attribute specifying the number of contiguous
 Segments with identical MPD duration minus one and an optional @t time attribute. The value of the @t
-attribute minus the value of the @presentationTimeOffset specifies the MPD start time of the firstpecifies the MPD start time of the first
+attribute minus the value of the @presentationTimeOffset specifies the MPD start time of the first
 Segment in the series.
 The @r attribute has a default value of zero (i.e., a single Segment in the series) when not present. For
 example, a repeat count of three means there are four contiguous Segments, each with the same MPD
